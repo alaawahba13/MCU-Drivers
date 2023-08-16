dcf49776ff3aa3185171aad3266b020f71115878
Alaa Wahba <alaawahbaa13@gmail.com>
2023-08-17 00:48:12 +0300

    LM35 driver

---
+++ ATMEGA32_Drivers/HAL/LM35/LM35.h
@@ -0,0 +1,19 @@
+/*
+ * LM35.h
+ *
+ *  Created on: 16 Aug 2023
+ *      Author: Alaa Wahba
+ */
+
+#ifndef LM35_LM35_H_
+#define LM35_LM35_H_
+
+#include "../../Atmega32/inc/ADC.h"
+
+#define ADCx 			ADC3
+
+void LM35_init();
+uint32 LM35_getTemperature();
+
+
+#endif /* LM35_LM35_H_ */
