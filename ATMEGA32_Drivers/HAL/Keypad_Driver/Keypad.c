0c4e4b884da52680262cc2524c76360b3223d04d
Alaa Wahba <alaawahbaa13@gmail.com>
2023-09-11 01:41:39 +0300

    Keypad

--- ATMEGA32_Drivers/HAL/Keypad_Driver/Keypad.c
+++ ATMEGA32_Drivers/HAL/Keypad_Driver/Keypad.c
@@ -17,55 +17,30 @@ void Keypad_init(){
 	 * The rows are input so if the button is pressed it's connected to GND
 	 *
 	 */
-	Keypad_DDR |= ( (1<<C0) | (1<<C1) |(1<<C2) |(1<<C3) );
-	Keypad_DDR &= ~((1<<R0) | (1<<R1) |(1<<R2) |(1<<R3) );
+	Keypad_COL_DDR |= ( (1<<C0) | (1<<C1) |(1<<C2) |(1<<C3) );
+	Keypad_ROW_DDR &= ~((1<<R0) | (1<<R1) |(1<<R2) |(1<<R3) );
 	/*
 	 * Initialize the PORT to 1 so the Cols are connected VCC
 	 * while the ROWs are input_pullup
 	 */
-	Keypad_PORT = 0xFF;
+	Keypad_Row_PORT |= ((1<<R0) | (1<<R1) |(1<<R2) |(1<<R3) );
+	Keypad_COL_PORT = 0xff;
 }
 char Keypad_Get_Key(){
 
 	int i, j;
+	char ret_key = 'A';
 	for(i =0 ; i< 4 ; i++){  // Rows loop
-		Keypad_PORT |= ((1<<Keypad_Cols[0]) | (1<<Keypad_Cols[1]) | (1<<Keypad_Cols[2]) | (1<<Keypad_Cols[3]));
-		Keypad_PORT &= ~(1<<Keypad_Cols[i]);
+		Keypad_COL_PORT |= ((1<<Keypad_Cols[0]) | (1<<Keypad_Cols[1]) | (1<<Keypad_Cols[2]) | (1<<Keypad_Cols[3]));
+		Keypad_COL_PORT &= ~(1<<Keypad_Cols[i]);
 		for(j =0 ; j< 4 ; j++){ // colws loop
-			if(!GET(Keypad_PIN, Keypad_Rows[j]))
+			if(!GET(Keypad_ROW_PIN, Keypad_Rows[j]))
 			{
-				while(!GET(Keypad_PIN, Keypad_Rows[j])); // single press
-					switch(i){
-					case 0:
-						if(j==0) 		return  '7';
-						else if(j==1) 	return  '4';
-						else if(j==2) 	return  '1';
-						else if(j==3) 	return  '?';
-						break;
-					case 1:
-						if(j==0) 		return  '8';
-						else if(j==1) 	return  '5';
-						else if(j==2) 	return  '2';
-						else if(j==3) 	return  '0';
-						break;
-					case 2:
-						if(j==0) 		return  '9';
-						else if(j==1) 	return  '6';
-						else if(j==2) 	return  '3';
-						else if(j==3) 	return  '=';
-						break;
-					case 3:
-						if(j==0) 		return  '/';
-						else if(j==1) 	return  '*';
-						else if(j==2) 	return  '-';
-						else if(j==3) 	return  '+';
-						break;
-
-					}
-
+				while(!GET(Keypad_ROW_PIN, Keypad_Rows[j])); // single press
+					ret_key = arr_keys[j][i];
 				}
 
 			}
 		}
-	return 'A';
+	return ret_key;
 }
--- ATMEGA32_Drivers/HAL/Keypad_Driver/Keypad.h
+++ ATMEGA32_Drivers/HAL/Keypad_Driver/Keypad.h
@@ -7,25 +7,39 @@
 
 #ifndef KEYPAD_DRIVER_KEYPAD_H_
 #define KEYPAD_DRIVER_KEYPAD_H_
-#include <../../Atmega32/inc/DIO.h>
+#include "../../Atmega32/inc/DIO.h"
 #include <util/delay.h>
 #include "../../Library/BIT_MATH.h"
 
 //Define the PORT , PIND & DDR register connected to keypad
-#define Keypad_PORT   		PORTC
-#define Keypad_DDR			DDRC
-#define Keypad_PIN 			PINC
+#define Keypad_Row_PORT   		PORTC
+#define Keypad_COL_PORT   		PORTD
+
+#define Keypad_ROW_DDR			DDRC
+#define Keypad_COL_DDR			DDRD
+
+#define Keypad_ROW_PIN 		    	PINC
 
 // Define the Keypad Cols & Rows pins
-#define R0 					0
-#define R1					1
-#define R2 					2
-#define R3					3
-#define C0 					4
-#define C1					5
-#define C2 					6
-#define C3					7
+#define R0 					5
+#define R1					4
+#define R2 					3
+#define R3					2
 
+#define C0 					7
+#define C1					6
+#define C2 					5
+#define C3					3
+
+//define keypad keys
+
+static const char arr_keys[4][4]=
+{{'7','8','9','/'},
+ {'4','5','6','*'},
+ {'1','2','3','-'},
+ {'?','0','=','+'}
+
+};
 
 void Keypad_init();
 char Keypad_Get_Key();
