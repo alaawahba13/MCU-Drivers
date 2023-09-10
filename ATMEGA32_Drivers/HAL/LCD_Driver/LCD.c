613051a6ed61db110a7becd1482dd1b7689bc91b
Alaa Wahba <alaawahbaa13@gmail.com>
2023-09-10 23:18:27 +0300

    LCD

--- ATMEGA32_Drivers/HAL/LCD_Driver/LCD.c
+++ ATMEGA32_Drivers/HAL/LCD_Driver/LCD.c
@@ -7,27 +7,19 @@
 
 #include "LCD.h"
 
-void isBusy(void){
-	// Set the data port as input
-	LCD_DDR &= ~(0xFF <<DATA_SHIFT) ;
-	// Set RW = 1 to read && Command mode >> RS =0
-	SET(LCD_CONTROL_PORT,ReadWrite);
-	CLEAR(LCD_CONTROL_PORT, REGISTER_SELECT);
-	// wait for the enable cycle
-	lcd_kick();
-	CLEAR(LCD_CONTROL_PORT, ReadWrite); //write
-}
-
-void lcd_init(){
+void lcd_init() {
 	// you must wait for the hardware to initialize
 	_delay_ms(20);
 	// set port as ouput to write commands
-	LCD_DDR= 0xFF;
+	SET(LCD_DDR, D4);
+	SET(LCD_DDR, D5);
+	SET(LCD_DDR, D6);
+	SET(LCD_DDR, D7);
+
 	// set three control pins as output and write 0
-	LCD_CONTROL_DDR |= (1<<ENABLE_SWITCH | 1<<REGISTER_SELECT | 1<<ReadWrite);
-	LCD_CONTROL_PORT &= ~(1<<ENABLE_SWITCH | 1<<REGISTER_SELECT | 1<<ReadWrite);
+	LCD_CONTROL_DDR |= (1 << ENABLE_SWITCH | 1 << REGISTER_SELECT);
+	LCD_CONTROL_PORT &= ~(1 << ENABLE_SWITCH | 1 << REGISTER_SELECT);
 	_delay_ms(2);
-	lcd_Clear_Screen();
 	/* Define the lcd Mode by its instruction*/
 #ifdef EIGHT_BIT_MODE
 	 lcd_Send_Command(FUNCTION_8BIT_2LINES);
@@ -35,39 +27,48 @@ void lcd_init(){
 
 #ifdef FOUR_BIT_MODE
 	// Command that initializes LCD as four bit mode
-	 lcd_Send_Command(0x02);
-	 lcd_Send_Command(FUNCTION_4BIT_2LINES);
+	lcd_Send_Command(0x02);
+	lcd_Send_Command(FUNCTION_4BIT_2LINES);
 #endif
-	 lcd_Send_Command(ENTRY_MODE);
-	 lcd_Send_Command(CURSOR_FIRST_LINE);
-	 lcd_Send_Command(DISPLAY_ON_CURSOR_BLINK);
+	lcd_Send_Command(ENTRY_MODE);
+	lcd_Send_Command(CURSOR_FIRST_LINE);
+	lcd_Send_Command(DISPLAY_ON_CURSOR_BLINK);
+	lcd_Clear_Screen();
 
 }
-void lcd_Send_Command(unsigned char command){
+void lcd_Send_Command(unsigned char command) {
 
 #ifdef EIGHT_BIT_MODE
 
 	// 1.Write
 	LCD_PORT = command;
 	// 2.Reset
-	LCD_CONTROL_PORT &= ~ ((1<<ReadWrite) | (1<< REGISTER_SELECT));
+	LCD_CONTROL_PORT &= ~ (1<< REGISTER_SELECT);
 	// 3. Reactivate Enable
 	lcd_kick();
 #endif
 #ifdef FOUR_BIT_MODE
 
-	LCD_PORT = (LCD_PORT & 0x0F) | (command & 0xF0);
-	LCD_CONTROL_PORT &= ~ ((1<<ReadWrite) | (1<< REGISTER_SELECT));
-	//_delay_ms(1);
+	//LCD_PORT =  (command & 0xF0);
+	GET(command,7) ? SET(LCD_PORT, D7) : CLEAR(LCD_PORT, D7);
+	GET(command,6) ? SET(LCD_PORT, D6) : CLEAR(LCD_PORT, D6);
+	GET(command,5) ? SET(LCD_PORT, D5) : CLEAR(LCD_PORT, D5);
+	GET(command,4) ? SET(LCD_PORT, D4) : CLEAR(LCD_PORT, D4);
+
+	LCD_CONTROL_PORT &= ~(1 << REGISTER_SELECT);
 	lcd_kick();
-	LCD_PORT = (LCD_PORT & 0x0F) | (command << 4);
-	LCD_CONTROL_PORT &= ~ ((1<<ReadWrite) | (1<< REGISTER_SELECT));
-	//_delay_ms(1);
+
+	GET(command,3) ? SET(LCD_PORT, D7) : CLEAR(LCD_PORT, D7);
+	GET(command,2) ? SET(LCD_PORT, D6) : CLEAR(LCD_PORT, D6);
+	GET(command,1) ? SET(LCD_PORT, D5) : CLEAR(LCD_PORT, D5);
+	GET(command,0) ? SET(LCD_PORT, D4) : CLEAR(LCD_PORT, D4);
+
+	LCD_CONTROL_PORT &= ~(1 << REGISTER_SELECT);
 	lcd_kick();
 #endif
 
 }
-void lcd_Send_Char(unsigned char character){
+void lcd_Send_Char(unsigned char character) {
 
 #ifdef EIGHT_BIT_MODE
 
@@ -75,69 +76,88 @@ void lcd_Send_Char(unsigned char character){
 	LCD_CONTROL_PORT |= (1<< REGISTER_SELECT);
 	//2. Write data
 	LCD_PORT = character;
-	LCD_CONTROL_PORT &= ~ (1<<ReadWrite);
 	// Reactivate Enable
 	lcd_kick();
 #endif
 #ifdef FOUR_BIT_MODE
-		LCD_PORT = (LCD_PORT & 0x0F ) | (character & 0xF0);
-		LCD_CONTROL_PORT |= (1<< REGISTER_SELECT);
-		LCD_CONTROL_PORT &= ~ (1<<ReadWrite);
-		//_delay_ms(1);
-		lcd_kick();
-		LCD_PORT = (LCD_PORT & 0x0F ) | (character <<4);
-		LCD_CONTROL_PORT |= (1<< REGISTER_SELECT);
-		LCD_CONTROL_PORT &= ~ (1<<ReadWrite);
-	//	_delay_ms(1);
-		lcd_kick();
+	GET(character,7) ? SET(LCD_PORT, D7) : CLEAR(LCD_PORT, D7);
+	GET(character,6) ? SET(LCD_PORT, D6) : CLEAR(LCD_PORT, D6);
+	GET(character,5) ? SET(LCD_PORT, D5) : CLEAR(LCD_PORT, D5);
+	GET(character,4) ? SET(LCD_PORT, D4) : CLEAR(LCD_PORT, D4);
+	LCD_CONTROL_PORT |= (1 << REGISTER_SELECT);
+
+	lcd_kick();
+	GET(character,3) ? SET(LCD_PORT, D7) : CLEAR(LCD_PORT, D7);
+	GET(character,2) ? SET(LCD_PORT, D6) : CLEAR(LCD_PORT, D6);
+	GET(character,1) ? SET(LCD_PORT, D5) : CLEAR(LCD_PORT, D5);
+	GET(character,0) ? SET(LCD_PORT, D4) : CLEAR(LCD_PORT, D4);
+	LCD_CONTROL_PORT |= (1 << REGISTER_SELECT);
+
+	lcd_kick();
 #endif
 }
-void lcd_send_String(char *string){
-   // keeps track of chars count
-	int count=0;
-	while(*string >0){
+void lcd_send_String(char *string) {
+	// keeps track of chars count
+	int count = 0;
+	while (*string > 0) {
 		count++;
 		lcd_Send_Char(*string++);
 		// if first line is full go to second
-		if (count == 16){
+		if (count == MAX_COLS) {
 			lcd_GOTO_XY(1, 0);
 		}
+#ifdef LCD_4x20
+		if (count == MAX_COLS * 2) {
+			lcd_GOTO_XY(2, 0);
+		} else if (count == MAX_COLS * 3) {
+			lcd_GOTO_XY(3, 0);
+		}
+#endif
+
 		// if both lines are full clear and start over.
-		else if(count== 32 ){
+		else if (count == (MAX_COLS * MAX_ROWS)) {
 			lcd_Clear_Screen();
 			lcd_GOTO_XY(0, 0);
-			count =0;
+			count = 0;
 		}
+
 	}
 }
-void lcd_GOTO_XY(unsigned char row , unsigned char col){
+void lcd_GOTO_XY(unsigned char row, unsigned char col) {
 
-	if(row ==0 )
-	{	if((col < 16) && (col>=0))
-		lcd_Send_Command(CURSOR_FIRST_LINE +col);
+	if (row == 0) {
+		if ((col < MAX_COLS) && (col >= 0))
+			lcd_Send_Command(CURSOR_FIRST_LINE + col);
+	} else if (row == 1) {
+		if ((col < MAX_COLS) && (col >= 0))
+			lcd_Send_Command(CURSOR_SECOND_LINE + col);
 	}
-	else if (row ==1)
-	{	if((col < 16) && (col>=0))
-		lcd_Send_Command(CURSOR_SECOND_LINE+col);
+#ifdef LCD_4x20
+	else if (row == 2) {
+		if ((col < MAX_COLS) && (col >= 0))
+			lcd_Send_Command(CURSOR_THIRD_LINE + col);
+	} else if (row == 3) {
+		if ((col < MAX_COLS) && (col >= 0))
+			lcd_Send_Command(CURSOR_FOURTH_LINE + col);
 	}
-
+#endif
 }
-void lcd_Clear_Screen(void){
+void lcd_Clear_Screen(void) {
 	lcd_Send_Command(CLEAR_SCREEN);
 }
-void lcd_display_number(int Number){
+void lcd_display_number(int Number) {
 	char str[7];
-	// Converts Int to String
-	sprintf(str,"%d",Number);
+// Converts Int to String
+	sprintf(str, "%d", Number);
 	lcd_send_String(str);
 }
-void lcd_display_Real_number(double Number){
+void lcd_display_Real_number(double Number) {
 	char str[16];
-	char *tmpSign  = (Number >0) ? "" : "-";
-	float tmpNum    = (Number >0) ? Number : -Number;
+	char *tmpSign = (Number > 0) ? "" : "-";
+	float tmpNum = (Number > 0) ? Number : -Number;
 
 	int tmpVal = tmpNum;
-	float tmpFrac  = tmpNum - tmpVal;
+	float tmpFrac = tmpNum - tmpVal;
 
 	int Frac = tmpFrac * 10000;
 
@@ -145,10 +165,10 @@ void lcd_display_Real_number(double Number){
 	lcd_send_String(str);
 
 }
-void lcd_kick(){
-	// Enable =0 >> LCD Busy
+void lcd_kick() {
+// Enable =0 >> LCD Busy
 	CLEAR(LCD_CONTROL_PORT, ENABLE_SWITCH);
-	_delay_ms(50);
+	_delay_ms(6);
 	SET(LCD_CONTROL_PORT, ENABLE_SWITCH);
 }
 
--- ATMEGA32_Drivers/HAL/LCD_Driver/LCD.h
+++ ATMEGA32_Drivers/HAL/LCD_Driver/LCD.h
@@ -7,28 +7,44 @@
 
 #ifndef LCD_H_
 #define LCD_H_
-#include <../../Atmega32/inc/DIO.h>
+#include "../../Atmega32/inc/DIO.h"
 #include <util/delay.h>
 #include "../../Library/BIT_MATH.h"
+#include "string.h"
 
 /*This is used for lcd data pins D0-D7 */
-#define LCD_PORT 				PORTA
-#define LCD_DDR  				DDRA
-/* This is used for three control pins RS, RW, Enable */
-#define LCD_CONTROL_PORT 		PORTB
-#define LCD_CONTROL_DDR  		DDRB
-// define the three control pins connected to MCU
-#define ENABLE_SWITCH			3
-#define ReadWrite				2
-#define REGISTER_SELECT         1
+#define LCD_PORT 				PORTB
+#define LCD_DDR  				DDRB
 
-#define EIGHT_BIT_MODE
-//#define FOUR_BIT_MODE
+
+#define D4 						PIN_0
+#define D5 						PIN_1
+#define D6 						PIN_2
+#define D7 						PIN_4
+
+
+/* This is used for three control pins RS, RW, Enable */
+#define LCD_CONTROL_PORT 		PORTA
+#define LCD_CONTROL_DDR  		DDRA
+// define the three control pins connected to MCU
+#define ENABLE_SWITCH			2
+#define REGISTER_SELECT         3
+
+//#define EIGHT_BIT_MODE
+#define FOUR_BIT_MODE
+
+#define LCD_4x20
+//#define LCD_2x16
+
+#define MAX_COLS				20
+#define MAX_ROWS				4
 
 #define DATA_SHIFT 				0
 /* Some instructions from data sheet */
 #define CURSOR_FIRST_LINE 		(0x80)
 #define CURSOR_SECOND_LINE 		(0xC0)
+#define CURSOR_THIRD_LINE 		(0x94)
+#define CURSOR_FOURTH_LINE 		(0xD4)
 
 #define FUNCTION_8BIT_2LINES 	(0x38)
 #define FUNCTION_4BIT_2LINES 	(0x28)
