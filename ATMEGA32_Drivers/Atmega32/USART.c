20e02aaf8b0066f4249003b27f7caccacf511f63
Alaa Wahba <alaawahbaa13@gmail.com>
2023-09-12 00:00:07 +0300

    USART

--- ATMEGA32_Drivers/Atmega32/USART.c
+++ ATMEGA32_Drivers/Atmega32/USART.c
@@ -7,14 +7,15 @@
 
 #include "inc/USART.h"
 #define Default_Stop 		'\r'
-static  uint32 flag=1;
+static uint32 flag = 1;
 static uint8 *TX_str;
 
-void USART_init(void) {
+void USART_init(UART_Config_t *pinConfig) {
 	/*   Baud rate   */
-	// For 9600 bps
-	UBRRL = 51;
-	CLEAR(UCSRA, U2X);
+	uint16 ubrr_value = (uint16) (((F_CPU / (pinConfig->baudRate * pinConfig->DoubleSpeed))) - 1);
+
+	UBRRH = ubrr_value >> 8;
+	UBRRL = ubrr_value;
 
 	/*  FRAME 	  */
 	//This bit selects  Asynchronous
@@ -25,9 +26,9 @@ void USART_init(void) {
 	//This bit selects the number of Stop Bits to be inserted by the Transmitter (1 bit)
 	CLEAR(UCSRC, USBS);
 	// sets the number of data bits (8 bits by default)
-	SET(UCSRC, UCSZ1);
-	SET(UCSRC, UCSZ0);
 	CLEAR(UCSRB, UCSZ2);
+	UCSRC = (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1);
+
 	/*   Enable  */
 	SET(UCSRB, RXEN);	//Writing this bit to one enables The USART Receiver.
 	SET(UCSRB, TXEN);	//Writing this bit to one enables The USART Transmitter.
@@ -36,7 +37,8 @@ void USART_init(void) {
 
 void USART_send(uint8 data) {
 	//The transmit buffer can only be written when the UDRE Flag in the UCSRA Register is set.
-	while (!GET(UCSRA, UDRE));
+	while (!GET(UCSRA, UDRE))
+		;
 	UDR = data;
 
 }
@@ -45,7 +47,8 @@ uint8 USART_recieve() {
 	 * This flag bit is set when there are unread data in the receive buffer
 	 * and cleared when the receive buffer is empty
 	 */
-	while (!GET(UCSRA, RXC));
+	while (!GET(UCSRA, RXC))
+		;
 	return UDR;
 
 }
@@ -66,7 +69,7 @@ uint32 USART_recieveNumber() {
 	 p[1] = USART_recieve();
 	 p[2] = USART_recieve();
 	 p[3] = USART_recieve();*/
-	 return  num;
+	return num;
 }
 void USART_sendString(uint8 *str) {
 	for (uint8 i = 0; i < str[i]; i++)
@@ -86,58 +89,58 @@ void USART_recieveString(uint8 *Buff) {
 	Buff[i] = '\0';
 }
 
-uint8 USART_recievePeriodicData(uint8 *data){
-	if(GET(UCSRA, RXC)){ // No blocking
+uint8 USART_recievePeriodicData(uint8 *data) {
+	if (GET(UCSRA, RXC)) { // No blocking
 		*data = UDR;
 		return 1; 	//As an indication that there's sent data
 	}
-	return 0 ; 			// When no data is sent
+	return 0; 			// When no data is sent
 }
 
 //Interrupt
 
 // Doesn't check if data is sent or not
-void USART_sendNoBlock(uint8 data){
-	UDR =data;
+void USART_sendNoBlock(uint8 data) {
+	UDR = data;
 }
-uint8 USART_recieveNoBlock(){
+uint8 USART_recieveNoBlock() {
 	return UDR;
 }
-void USART_TX_interrupt_Enable(){
-	SET(UCSRB,TXCIE);
+void USART_TX_interrupt_Enable() {
+	SET(UCSRB, TXCIE);
 }
-void USART_TX_interrupt_Disable(){
-	CLEAR(UCSRB,TXCIE);
-}
-
-void USART_RX_interrupt_Enable(){
-	SET(UCSRB,RXCIE);
-}
-void USART_RX_interrupt_Disable(){
-	CLEAR(UCSRB,RXCIE);
-
-}
-void USART_send_string_Asynch(uint8 *str){
-    if(flag == 1){
-    	 USART_sendNoBlock(str[0]);
-    	   TX_str = str;
-        flag =0;
-   	 USART_TX_interrupt_Enable();
-
-    }
+void USART_TX_interrupt_Disable() {
+	CLEAR(UCSRB, TXCIE);
 }
 
-void __vector_15 (void)		__attribute__ ((signal)) ;
-void __vector_15 (void){
-		static uint8 i =1;
-		if(TX_str[i] != '\0'){
-			USART_sendNoBlock(TX_str[i]);
-			  i++;
+void USART_RX_interrupt_Enable() {
+	SET(UCSRB, RXCIE);
+}
+void USART_RX_interrupt_Disable() {
+	CLEAR(UCSRB, RXCIE);
 
-		}else{
-			i =1 ;
-			flag = 1; // Flag is set to one when transmit is complete.
+}
+void USART_send_string_Asynch(uint8 *str) {
+	if (flag == 1) {
+		USART_sendNoBlock(str[0]);
+		TX_str = str;
+		flag = 0;
+		USART_TX_interrupt_Enable();
+
+	}
+}
+
+void __vector_15(void) __attribute__ ((signal));
+void __vector_15(void) {
+	static uint8 i = 1;
+	if (TX_str[i] != '\0') {
+		USART_sendNoBlock(TX_str[i]);
+		i++;
+
+	} else {
+		i = 1;
+		flag = 1; // Flag is set to one when transmit is complete.
 		//	USART_TX_interrupt_Disable();
 
-		}
+	}
 }
--- ATMEGA32_Drivers/Atmega32/inc/USART.h
+++ ATMEGA32_Drivers/Atmega32/inc/USART.h
@@ -14,13 +14,44 @@
 #include "../../Library/BIT_MATH.h"
 #include "../../HAL/LCD_Driver/lcd.h"
 
+typedef struct{
+	uint8 baudRate;   // @ref USART_BaudRate_define
+	uint8 DoubleSpeed;
+}UART_Config_t;
+
+
+/*
+ * @ref USART_BaudRate_define
+ */
+#define USART_BaudRate_2400						2400
+#define USART_BaudRate_4800						4800
+#define USART_BaudRate_9600						9600
+#define USART_BaudRate_14400					14400
+#define USART_BaudRate_19200					19200
+#define USART_BaudRate_28800					28800
+#define USART_BaudRate_38400					38400
+#define USART_BaudRate_57600					57600
+#define USART_BaudRate_76800					76800
+#define USART_BaudRate_115200					115200
+#define USART_BaudRate_230400					230400
+#define USART_BaudRate_250000					250000
+
+/*
+ * @ref Double_speed
+ */
+
+#define DOUBLE_Speed_EN							8UL
+#define DOUBLE_Speed_EN							16UL
+
+
 /*
 * ===============================================
 * APIs Supported by "MCAL DIO DRIVER"
 * ===============================================
 */
 
-void USART_init(void);
+
+void USART_init(UART_Config_t *pinConfig);
 void USART_send(uint8 data);
 uint8 USART_recieve();
 
